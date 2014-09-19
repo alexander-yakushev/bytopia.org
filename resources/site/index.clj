1709706239f4c35b3900a5a91f8d79724c1a3b25
Alexander Yakushev <alex@bytopia.org>
2014-09-19 23:08:03 +0200

    Redesign blog to Avenue

--- resources/site/index.clj
+++ resources/site/index.clj
@@ -5,22 +5,26 @@
  (for [f (take 10 (reverse (io/list-files :posts)))
        :let [url (post-url f)
              [metadata content] (io/read-doc f)]]
-   [:div.entry-preview
-    [:h2.entry-title
-     [:a {:href url} (:title metadata)]]
+   [:div.post-outer
+    [:div.post.hentry
+     [:div.bposttitle
+      [:h2.post-title.entry-title
+       [:a {:href url} (:title metadata)]]
 
-    [:div.entry-info
-     [:span.entry-date (format-date (:date metadata) "dd MMMM YYYY")]
-     [:span.entry-tags "Tags: "
-      (->> (:tags metadata)
-           (map (fn [tag] [:a {:href (str "/tags/#" tag)} tag]))
-           (interpose " "))]
-     [:div.clear]]
-
-    [:div.entry-content
-     (if-let [synopsis (:synopsis metadata)]
-       (list synopsis [:p.readmore
-                       [:a {:href url} "Read more ->>"]])
-       content)]])
+      [:div.entry-meta
+       [:span.entry-date (format-date (:date metadata) "dd MMMM YYYY")]
+       [:span.entry-tags "Tags: "
+        (->> (:tags metadata)
+             (map (fn [tag] [:a {:href (str "/tags/#" tag)} tag]))
+             (interpose " "))]
+       [:div.clear]]]
+     [:div.entry
+      [:div.post-body.entry-summary
+       (if-let [^String synopsis (:synopsis metadata)]
+         (let [lastp (.lastIndexOf synopsis "<p>")]
+           (list (if-not (= lastp (.indexOf synopsis "<p>"))
+                   (subs synopsis 0 lastp) synopsis)
+                 [:p.readmore
+                  [:a {:href url} "Read more ->>"]])))]]]])
 
  [:p "For more posts see the " [:a {:href "/archives.html"} "Archives"] " page."])
--- resources/templates/default.clj
+++ resources/templates/default.clj
@@ -3,9 +3,12 @@
                           now (+ (.getYear (java.util.Date.)) 1900)]
                       (if (= created now)
                         (str created)
-                        (str created "-" now)))]
-  [:html
-   {:xmlns "http://www.w3.org/1999/xhtml", :lang "en", :xml:lang "en"}
+                        (str created "-" now)))
+      include-css (fn [link] [:link {:rel "stylesheet", :type "text/css",
+                                    :href link}])]
+
+  [:html {:xmlns "http://www.w3.org/1999/xhtml", :lang "en", :xml:lang "en"}
+
    [:head
     [:meta {:http-equiv "content-type", :content "text/html; charset=UTF-8"}]
     [:meta {:name "description", :content (:description metadata)}]
@@ -13,122 +16,133 @@
     [:meta {:name "author", :content (:site-author config)}]
     [:link {:rel "icon", :href "/images/favicon.ico" :type "image/x-icon"}]
     [:link {:rel "shortcut icon",:href "/images/favicon.ico" :type "image/x-icon"}]
-    [:link {:rel "stylesheet", :type "text/css", :href "/default.css"}]
-    [:link {:rel "stylesheet", :type "text/css", :href "/tomorrow_night.css"}]
+    (include-css "/default.css")
+    (include-css "/tomorrow_night.css")
+    (include-css "http://fonts.googleapis.com/css?family=Oswald")
+    (include-css "http://fonts.googleapis.com/css?family=Pathway+Gothic+One")
     (when (= (:title metadata) "Home")
-      [:link {:rel "stylesheet", :type "text/css", :href "/goodreads.css"}])
-    [:link
-     {:rel "alternate", :type "application/rss+xml",
-      :title (:site-title config), :href "/rss-feed"}]
-
+      (include-css "/goodreads.css"))
+    [:link {:rel "alternate", :type "application/rss+xml",
+            :title (:site-title config), :href "/rss-feed"}]
     (if (= (:type metadata) :post)
-      [:link {:rel "canonical"
-              :href (str (:site-url config) (:url metadata))}])
+      [:link {:rel "canonical" :href (str (:site-url config) (:url metadata))}])
     [:title (str (:title metadata) " - " (:site-title config))]]
+
    [:body
-    [:div {:id "header"}
-     [:h1.site-name
-      [:a {:href "/"} "Bytopia"]]
-     [:p.site-description (:site-description config)]]
-    [:div.clear]
-    [:div#content
-     [:div#outbox
-      [:div#navigation
-       [:div#pages
-        (let [make-link (fn [link-name & {:keys [link title]
-                                         :or {link (str "/" (.toLowerCase link-name) ".html")
-                                              title link-name}}]
-                          [:li (if (and (= (:type metadata) :site)
-                                        (= (:title metadata) title))
-                                 {:class "current_page_item"} {})
-                           [:a {:href link} link-name]])]
-          [:ul
-           (make-link "Home" :link "/")
-           (make-link "About")
-           (make-link "Projects")
-           (make-link "Tags" :link "/tags/")
-           (make-link "Archives")])
-        [:div.clear]]
-       [:form#searchform {:action "http://www.google.com/search",
-                          :method "get"}
-        [:div
-         [:label.screen-reader-text {:for "s"} "Search for:"]
-         [:input#s {:name "q", :value "", :type "text"}]
+    [:div#masthead
+     [:div#head
+      [:div#top.clearfix
+       [:div#blogname
+        [:div#header.header.section
+         [:div#Header1.widget.Header
+          [:div#header-inner
+           [:div.titlewrapper
+            [:h1.title
+             [:a {:href "/"} "Bytøpia"]]]
+           [:div.descriptionwrapper
+            [:p.description (:site-description config)]]]]]]
+
+       [:div#contactlist
+        [:a {:href "/rss-feed"} [:img {:src "/images/icons/rss.png"}]]
+        [:a {:href "https://github.com/alexander-yakushev"} [:img {:src "/images/icons/github.png"}]]
+        [:a {:href "mailto:alex@bytopia.org"} [:img {:src "/images/icons/email.png"}]]
+        [:a {:href "https://plus.google.com/u/0/107472102450770404696"} [:img {:src "/images/icons/gplus.png"}]]
+        [:a {:href "https://www.linkedin.com/profile/view?id=82917611"} [:img {:src "/images/icons/linkedin.png"}]]
+        [:a {:href "https://twitter.com/unlog1c"} [:img {:src "/images/icons/twitter.png"}]]]]
+
+      [:div#botmenu
+       [:div#submenu.menu-primary-container
+        [:div#nbttopmenu.nbttopmenu.section
+         [:div#PageList8.widget.PageList
+          [:div.widget-content
+           (let [make-link (fn [link-name & {:keys [link title]
+                                            :or {link (str "/" (.toLowerCase link-name) ".html")
+                                                 title link-name}}]
+                             [:li (if (and (= (:type metadata) :site)
+                                           (= (:title metadata) title))
+                                    {:class "current_page_item"} {})
+                              [:a {:href link} link-name]])]
+             [:ul#menu-primary.sfmenu
+              (make-link "Home" :link "/")
+              (make-link "About")
+              (make-link "Projects")
+              (make-link "Tags" :link "/tags/")
+              (make-link "Archives")])
+           [:div.clear]]]]]
+       [:div#search
+        [:form#searchform {:action "http://www.google.com/search", :method "get"}
          [:input {:type "hidden" :name "sitesearch", :value "www.bytopia.org"}]
-         [:input#searchsubmit {:value "Search", :type "submit"}]]]]
-      [:div.clear]
-      [:div#main
-       [:div.post
-        (if (and (or (= (:type metadata) :post)
-                     (= (:type metadata) :site))
-                 (not (:skip-title metadata)))
-          [:h2.entry-title (:title metadata)])
+         [:input {:id "s", :name "q",  :value "", :type "text"
+                  :onblur "if(this.value==''){this.value='search site'};"
+                  :onfocus "if(this.value=='search site'){this.value=''};"}]]]
+       [:div.clear]]]]
 
-        (if (= (:type metadata) :post)
-          [:div.entry-info
-           [:span.entry-date
-            (format-date (:date metadata) "dd MMMM YYYY")]
-           (reduce
-            (fn[h v]
-              (conj h [:a {:href (str "/tags/#" v)} v] " "))
-            [:span.entry-tags "Tags: "]
-            (:tags metadata))
-           [:div.clear]])
+    [:div#wrapper
+     [:div#casing
+      [:div#content
+       [:div#mainblog.mainblog.section
+        [:div#Blog1.widget.blog
+         [:div.blog-posts.hfeed
+          [:div.date-outer
+           [:div.date-posts
+            (if-not (= (:title metadata) "Home")
+              [:div.post-outer
+               [:div.post.hentry
+                [:div.bposttitle
+                 [:h2.post-title.entry-title (:title metadata)]
+                 (when (= (:type metadata) :post)
+                   [:div.entry-meta
+                    [:span.entry-date (format-date (:date metadata) "dd MMMM YYYY")]
+                    [:span.entry-tags "Tags: "
+                     (->> (:tags metadata)
+                          (map (fn [tag] [:a {:href (str "/tags/#" tag)} tag]))
+                          (interpose " "))]
+                    [:div.clear]])]
 
-        content
+                [:div.entry
+                 [:div.post-body.entry-summary
+                  content
+                  (when (= (:type metadata) :post)
+                    [:div#comments
+                     [:script {:src "/juvia.js", :type "text/javascript"}]])]]]]
 
-        (when (= (:type metadata) :post)
-          [:div#comments
-           [:script {:src "/juvia.js", :type "text/javascript"}]])]]
-
-      [:div#sidebar
-       [:div
-        [:h2.title "Recent posts"]
-        [:ul#recent-posts
-         (for [f (take 5 (reverse (io/list-files :posts)))
-               :let [[metadata _] (io/read-doc f)]]
-           [:li [:a {:href (post-url f)} (:title metadata)]])]]
-       [:div
-        [:h2.title "Tags"]
-        [:ul {}
-         (reduce (fn [div tag-kw]
-                   (conj div [:a {:href (str "/tags/#" (name tag-kw))}
-                              (name tag-kw)] " "))
-                 [:li#taglist]
-                 (keys (tag-map)))
-         ]]
-       [:div#links
-        [:h2.title "Links"]
-        [:div#links-left
-         [:ul
-          [:li [:a#rss-feed {:href "/rss-feed"} "RSS Feed"]]
-          [:li [:a#github {:href "https://github.com/alexander-yakushev"} "Github"]]
-          [:li [:a#gplus {:href "https://plus.google.com/u/0/107472102450770404696"} "Google+"]]]]
-        [:div#links-right
-         [:ul
-          [:li [:a#email {:href "mailto:alex@bytopia.org"} "E-mail"]]
-          [:li [:a#linkedin {:href "https://www.linkedin.com/profile/view?id=82917611"} "LinkedIn"]]
-          [:li [:a#goodreads {:href "https://www.goodreads.com/user/show/5694609-alexander-yakushev"} "Goodreads"]]]]
-        [:div.clear]]
-       (when (= (:title metadata) "Home")
-         [:div#widgets
-          [:div#goodreads
-           [:script {:type "text/javascript", :charset "utf-8"
-                     :src "https://www.goodreads.com/review/custom_widget/5694609.Recent%20books?cover_position=left&cover_size=small&num_books=5&order=d&shelf=read&show_author=1&show_cover=1&show_rating=0&show_review=0&show_tags=1&show_title=1&sort=date_read&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=13821383570&widget_text_color=000000&widget_title_size=medium&widget_width=medium"}]]
-          [:div.clear]
-          [:h2.title "Featured tracks"]
-          [:div {:style "margin: 10px;"}
-           [:iframe {:id "widget", :scrolling "no", :frameborder 0, :width 293, :height 470,
-                     :style "width: 293px; height: 470px;"
-                     :src "//widgets.jamendo.com/v3/playlist/89122293?autoplay=0&layout=cover&manualWidth=293&width=293&theme=light&highlight=7&tracklist=true&tracklist_n=3&embedCode="}]]])
-       ]]]
+              content)]]]]]]
+      [:div#right
+       [:div.sidebar
+        [:div#sidebartop.sidebartop.section
+         [:div.widget.HTML
+          [:h2.title "Recent posts"]
+          [:div.widget-content
+           [:ul#recent-posts
+            (for [f (take 10 (reverse (io/list-files :posts)))
+                  :let [[metadata _] (io/read-doc f)]]
+              [:li [:a {:href (post-url f)} (:title metadata)]])]]]
+         [:div.widget.HTML
+          [:h2.title "Tags"]
+          [:div.widget-content
+           [:ul#taglist {}
+            (reduce (fn [div tag-kw]
+                      (conj div [:a {:href (str "/tags/#" (name tag-kw))}
+                                 (name tag-kw)] " "))
+                    [:li#taglist]
+                    (keys (tag-map)))
+            ]]]
+         (when (= (:title metadata) "Home")
+           [:div.widget.HTML
+            [:div#widgets
+             [:div#goodreads
+              [:script {:type "text/javascript", :charset "utf-8"
+                        :src "https://www.goodreads.com/review/custom_widget/5694609.Recent%20books?cover_position=left&cover_size=small&num_books=5&order=d&shelf=read&show_author=1&show_cover=1&show_rating=0&show_review=0&show_tags=1&show_title=1&sort=date_read&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=13821383570&widget_text_color=000000&widget_title_size=medium&widget_width=medium"}]]
+             [:div.clear]
+             ]])
+         ]]]
+      [:div.clear]]]
     [:div#footer
-     [:div#footerbox
-      [:div.support
-       "&copy; " blog-timespan " Alexander Yakushev"
-       [:p ;; {:class "support"}
-        "Powered by " [:a {:href "https://github.com/alexander-yakushev/discharge"} "Discharge"]
-        " | Theme " [:a {:href "http://axiu.me"} "mxs"]]]]]
+     [:div.fcred "&copy; " blog-timespan " Alexander Yakushev"
+      [:p.support "Powered by " [:a {:href "https://github.com/alexander-yakushev/discharge"} "Discharge"]
+       " | Theme " [:a {:href "http://avenue-btemplates.blogspot.no/"} "Avenue"]
+       " | Icons by " [:a {:href "https://dribbble.com/LeeGargano"} "LeeGargano"]]]
+     [:div.clear]]
 
     "<!-- Piwik -->
 <script type=\"text/javascript\">
